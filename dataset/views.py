03fd0a141b1ea9d3638419bddd3559109b61dd63
omondi254 <maildon254@gmail.com>
2023-03-30 10:09:20 +0300

    added templates & edited views

--- dataset/views.py
+++ dataset/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'dataset/index.html')
+
+
+
+def about(request):
+    pass
+
+
+def contact(request):
+    pass
+
+
+def Groups(request):
+    pass
